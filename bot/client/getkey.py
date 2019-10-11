d80afdaf6d4b372d55a334196a0698e1598f08d4
UnsignedByte <edlam1889@gmail.com>
2019-10-11 13:34:38 -0700

    fix environ

--- bot/client/getkey.py
+++ bot/client/getkey.py
@@ -3,7 +3,7 @@ import sys
 
 def readKey():
     if not os.path.exists("Data/keys/key.txt"):
-        return [os.environ('DISCORD_KEY'), os.environ('IMGUR_KEY'), os.environ('WOLFRAMALPHA_KEY')]
+        return [os.environ['DISCORD_KEY'], os.environ['IMGUR_KEY'], os.environ['WOLFRAMALPHA_KEY']]
     return [open("Data/keys/key.txt", 'r').read().strip(),
             open("Data/keys/imgurkey.txt", 'r').read().strip(),
             open("Data/keys/wolframalphakey.txt", 'r').read().strip()]
